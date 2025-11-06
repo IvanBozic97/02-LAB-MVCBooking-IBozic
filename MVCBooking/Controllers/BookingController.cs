6b074958cc13c22b1ece982986cab0c59b516d03
Ivan Božić <ivbozic@vsite.hr>
2025-11-06 19:03:01 +0100

    Dodan folder booking prebacen index i crete u booking te dodan index na home Ispravljeni linkovi saljem ispravljenu vjezbu - IBozic

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+using System.Collections.Generic;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        // Privremena lista bookinga
+        private static List<HotelBooking> _bookings = new List<HotelBooking>();
+        private static int _nextID = 1;
+
+        // GET: /Booking
+        public IActionResult Index()
+        {
+            // Ako je bookings null, inicijaliziraj praznom listom
+            return View(_bookings);
+        }
+
+
+        // GET: /Booking/Create
+        public IActionResult Create()
+        {
+            return View(new HotelBooking());
+        }
+
+        // POST: /Booking/CreateBooking
+        [HttpPost]
+        public IActionResult CreateBooking(HotelBooking booking)
+        {
+            if (!ModelState.IsValid || string.IsNullOrWhiteSpace(booking.GuestName) || booking.RoomNumber <= 0)
+            {
+                return View("Create", booking);
+            }
+
+            booking.Id = _nextID++;
+            _bookings.Add(booking);
+            return RedirectToAction("Index");
+        }
+    }
+}
